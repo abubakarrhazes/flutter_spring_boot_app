44a4ca2bb40dd4a0b4529eb233d73d7b008511c9
alrhazes <anuuman2@gmail.com>
2024-01-10 13:14:57 +0100

    initial commit

---
+++ src/main/java/com/example/flutter_spring_boot_app/FlutterSpringBootAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.flutter_spring_boot_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlutterSpringBootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlutterSpringBootAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/flutter_spring_boot_app/FlutterSpringBootAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.flutter_spring_boot_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlutterSpringBootAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
